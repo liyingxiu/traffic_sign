8dc5cf64b472c1fc4ac7cd93976fc1ed532200ca
yingxiu <200810048a@163.com>
2019-03-25 15:53:04 +0800

    delet everthing about regularizer

--- net_model.py
+++ net_model.py
@@ -1,7 +1,7 @@
 # 建立训练时的网络模型
 
 import tensorflow as tf
-regularizer = 0.0001
+#regularizer = 0.0001
 
 
 def inference(images, batch_size, n_classes):
@@ -10,7 +10,7 @@ def inference(images, batch_size, n_classes):
     with tf.variable_scope('conv1') as scope:
         weights = tf.get_variable('weights', shape=[5, 5, 3, 32], dtype=tf.float32,
                                   initializer=tf.contrib.layers.xavier_initializer(stddev=0.1, dtype=tf.float32))
-        tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
+        #tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
         biases = tf.get_variable('biases', shape=[32], dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(images, weights, strides=[1, 1, 1, 1], padding='SAME')
@@ -23,7 +23,7 @@ def inference(images, batch_size, n_classes):
     with tf.variable_scope('conv2') as scope:
         weights = tf.get_variable('weights', shape=[5, 5, 32, 64], dtype=tf.float32,
                                   initializer=tf.contrib.layers.xavier_initializer(stddev=0.1, dtype=tf.float32))
-        tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
+        #tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
         biases = tf.get_variable('biases', shape=[64], dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         conv = tf.nn.conv2d(pool1, weights, strides=[1, 1, 1, 1], padding='SAME')
@@ -40,7 +40,7 @@ def inference(images, batch_size, n_classes):
         dim = reshape.get_shape()[1].value
         weights = tf.get_variable('weights', shape=[dim, 128], dtype=tf.float32,
                                   initializer=tf.contrib.layers.xavier_initializer(stddev=0.1, dtype=tf.float32))
-        tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
+        #tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
         biases = tf.get_variable('biases', shape=[128], dtype=tf.float32, initializer=tf.constant_initializer(0.1))
         local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
 
@@ -48,7 +48,7 @@ def inference(images, batch_size, n_classes):
     with tf.variable_scope('local4') as scope:
         weights = tf.get_variable('weights', shape=[128, 84], dtype=tf.float32,
                                   initializer=tf.contrib.layers.xavier_initializer(stddev=0.1, dtype=tf.float32))
-        tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
+        #tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
         biases = tf.get_variable('biases', shape=[84], dtype=tf.float32, initializer=tf.constant_initializer(0.1))
         local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
 
@@ -56,7 +56,7 @@ def inference(images, batch_size, n_classes):
     with tf.variable_scope('output_layer') as scope:
         weights = tf.get_variable('weights', shape=[84, n_classes], dtype=tf.float32,
                                   initializer=tf.contrib.layers.xavier_initializer(stddev=0.01, dtype=tf.float32))
-        tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
+        #tf.add_to_collection('loss_w', tf.contrib.layers.l2_regularizer(regularizer)(weights))
         biases = tf.get_variable('biases', shape=[n_classes], dtype=tf.float32,
                                  initializer=tf.constant_initializer(0.1))
         output = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
@@ -72,7 +72,7 @@ def losses(prediction, labels):
                                                                        name='x_entropy_per_example')
         # 求cross_entropy所有元素的平均值
         loss = tf.reduce_mean(cross_entropy, name='loss')
-        loss = loss + tf.add_n(tf.get_collection('loss_w'))
+        #loss = loss + tf.add_n(tf.get_collection('loss_w'))
         # 对loss值进行标记汇总，一般在画loss, accuracy时会用到这个函数。
         tf.summary.scalar(scope.name, loss)
     return loss
